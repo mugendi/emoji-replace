e93eeb556822d7e60fca044e4893082783299e12
Anthony Mugendi <Anthony Mugendi>
2016-03-06 21:42:19 +0300

    First Commit

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 var emoji_replace = require('./index');
 
+//the string containing emoticons
 var str = 'my ;-) and :heart: :) ';
 
+//now some replacements.
+//Notice this function returns a promise so you can resolve without async hell :-)
 emoji_replace(str).then(function(response) {
 
+  //return replaced text
+  //if you would rather return an object containing both odl and new string, then call 'emoji_replace(str,true)'
   console.log( JSON.stringify(response,0,4) );
 
-}, function(error) {
-  console.error( error);
 });
